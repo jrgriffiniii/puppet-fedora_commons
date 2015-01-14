989fe4d723cfb1ed2e0e7a9b56a504b7c4ca6b28
jrgriffiniii <griffinj@lafayette.edu>
2015-01-14 11:29:14 -0500

    DSS-424 #time 17m Restructuring resource requirements for the installation of Fedora Commons; Resolving syntax errors for certain variables

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,7 +13,7 @@ class fedora_commons::install inherits fedora_commons {
 
   exec { 'fedora_commons_set_env':
 
-    command => "/usr/bin/env bash -c \"export FEDORA_HOME=${fedora_common::home}\" "
+    command => "/usr/bin/env bash -c \"export FEDORA_HOME=${fedora_commons::home} CATALINA_HOME=${fedora_commons::fedora_servlet_home}\""
   }
 
   class { 'postgresql::globals':
@@ -35,17 +35,17 @@ class fedora_commons::install inherits fedora_commons {
   
   # Create the database for the repository
   # Use either MySQL or PostgreSQL
-  postgresql::server::role { $fedora_common::database_user:
+  postgresql::server::role { $fedora_commons::database_user:
     
-    password_hash => postgresql_password($fedora_common::database_user, $fedora_common::database_pass),
+    password_hash => postgresql_password($fedora_commons::database_user, $fedora_commons::database_pass),
     require => Class['postgresql::server']
   }
     
-  postgresql::server::db { $fedora_common::database:
+  postgresql::server::db { $fedora_commons::database:
     
-    user     => $fedora_common::database_user,
-    password => postgresql_password($fedora_common::database_user, $fedora_common::database_pass),
-    require => Postgresql::Server::Role[$fedora_common::database_user]
+    user     => $fedora_commons::database_user,
+    password => postgresql_password($fedora_commons::database_user, $fedora_commons::database_pass),
+    require => Postgresql::Server::Role[$fedora_commons::database_user]
   }
 
   exec { 'fedora_commons_download':
@@ -54,17 +54,22 @@ class fedora_commons::install inherits fedora_commons {
     unless => '/usr/bin/env stat /tmp/fcrepo-installer-3.8.0.jar'
   }
 
+  file { $fedora_commons::home:
+
+    directory => true
+  }
+
   file { "${fedora_commons}/install/install.properties":
 
     content => template('fedora_commons/install.properties.erb'),
-    require => Exec['fedora_commons_set_env', 'fedora_commons_download', 'fedora_commons_create_database']
+    require => File[$fedora_commons::home]
   }
 
   exec { 'fedora_commons_install':
 
     command => "/usr/bin/env java ${fedoragsearch::download_url} -O /tmp/fcrepo-installer-3.8.0.jar ${fedora_commons}/install/install.properties",
     unless => "/usr/bin/env stat ${fedoragsearch::home}",
-    require => File["${fedora_commons}/install/install.properties"]
+    require => [ Exec['fedora_commons_set_env', 'fedora_commons_download' ], File["${fedora_commons}/install/install.properties"], Postgresql::Server::Db[$fedora_commons::database] ]
   }
 
   exec { 'fedoragsearch_deploy':
